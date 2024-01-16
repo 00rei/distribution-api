f11a7c83acebe29649124375d4ed5fc40ebbca35
Verstunin Andrey <verstunin.00@gmail.com>
2024-01-16 14:08:04 +0700

    Update table 'couriers' & create tables 'districts', 'orders', 'couriers_districts'

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from .config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
--- src/models.py
+++ src/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, UUID, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, UUID, String, DateTime, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.functions import now
 
 from .database import Base
 
@@ -8,3 +10,40 @@ class Courier(Base):
 
     id = Column(UUID, primary_key=True)
     name = Column(String)
+    avg_order_complete_time = Column(DateTime)
+    avg_day_orders = Column(Float)
+
+    orders = relationship("Order", back_populates="courier")
+    courier_districts = relationship("District", back_populates="district_couriers", secondary="couriers_districts")
+
+
+class District(Base):
+    __tablename__ = "districts"
+
+    id = Column(UUID, primary_key=True)
+    name = Column(String)
+
+    orders = relationship("Order", back_populates="district")
+    district_couriers = relationship("Courier", back_populates="courier_districts", secondary="couriers_districts")
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    id = Column(UUID, primary_key=True)
+    name = Column(String)
+    district_id = Column(UUID, ForeignKey("districts.id"))
+    courier_id = Column(UUID, ForeignKey("couriers.id"))
+    status = Column(Integer)
+    date_publication = Column(DateTime, default=now)
+    date_completion = Column(DateTime, default=None)
+
+    district = relationship("District", back_populates="orders")
+    courier = relationship("Courier", back_populates="orders")
+
+
+class CourierDistrict(Base):
+    __tablename__ = "couriers_districts"
+
+    courier_id = Column(UUID, ForeignKey("couriers.id"), primary_key=True)
+    district_id = Column(UUID, ForeignKey("districts.id"), primary_key=True)
