596e502378d72e83d40e88c35bf2b3c903d37282
Verstunin Andrey <verstunin.00@gmail.com>
2024-01-19 04:41:50 +0700

    Add README.md & fix CRLF/LF

--- docker/app.sh
+++ docker/app.sh
@@ -2,4 +2,6 @@
 
 alembic upgrade head
 
+pytest -s -v -p no:warnings
+
 uvicorn src.main:app --host 0.0.0.0 --port 8000
--- src/database.py
+++ src/database.py
@@ -4,16 +4,16 @@ from .config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 from .config import DB_HOST_TEST, DB_PORT_TEST, DB_USER_TEST, DB_NAME_TEST, DB_PASS_TEST
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SQLALCHEMY_DATABASE_URL_TEST = f"postgresql://{DB_USER_TEST}:{DB_PASS_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
-engine_test = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 
 def get_session(mode: str = 'dev') -> Session:
     if mode == 'dev':
         SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     else:
+        engine_test = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
         SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine_test)
 
     return SessionLocal()
@@ -23,8 +23,10 @@ Base = declarative_base()
 
 
 def drop_test_table() -> None:
+    engine_test = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
     Base.metadata.drop_all(bind=engine_test)
 
 
 def create_test_table() -> None:
+    engine_test = create_engine(SQLALCHEMY_DATABASE_URL_TEST)
     Base.metadata.create_all(bind=engine_test)
